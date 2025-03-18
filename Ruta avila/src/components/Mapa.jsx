6642861dc28e49e8ae219d44de3711735ed00c91
tomasparaco <tomas.paraco@correo.unimet.edu.ve>
2025-03-17 20:18:52 -0400

    Add files via upload

---
+++ Ruta avila/src/components/Mapa.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Mapa() {
+  return (
+    <div className="map-card">
+      <div className="header">
+        <img src="src\assets\homepage\image-11-44.png" alt="map" />
+      </div>
+    </div>
+  );
+}
